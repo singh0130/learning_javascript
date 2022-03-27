ac99dbb27dbae4e9c293a5b251d2c3225b689378
singh0130 <mr.mohitsinghdev@gmail.com>
2022-03-27 20:57:19 +0530

    task 4 solution added

--- dom.js
+++ dom.js
@@ -28,3 +28,18 @@ headertitle.innerHTML='<h3>Hello</h3';
 let additems=document.querySelector('.title');
 additems.style.color='green';
 additems.style.fontWeight='bold'; 
+
+let items=document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent='Hello 2';
+items[2].style.backgroundColor='green';
+/*
+for(let i=0;i<items.length;i++)
+{
+    items[i].style.fontWeight='bold';
+}
+*/
+Array.from(items).forEach(function(item){
+    item.style.fontWeight='bold';
+});
