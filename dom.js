c63a12782fcc226132a15fe4aa76a3a756e2c547
singh0130 <mr.mohitsinghdev@gmail.com>
2022-03-28 10:39:22 +0530

    dom task 6 solution added

--- dom.js
+++ dom.js
@@ -24,7 +24,7 @@ headertitle.innerHTML='<h3>Hello</h3';
 */
 
 //headertitle.style.borderBottom='solid 3px #000';
-
+/*
 let additems=document.querySelector('.title');
 additems.style.color='green';
 additems.style.fontWeight='bold'; 
@@ -34,24 +34,67 @@ console.log(items);
 console.log(items[1]);
 items[1].textContent='Hello 2';
 items[2].style.backgroundColor='green';
-
+*/
 /*
 for(let i=0;i<items.length;i++)
 {
     items[i].style.fontWeight='bold';
 }
 */
-
+/*
 Array.from(items).forEach(function(item){
     item.style.fontWeight='bold';
 });
+*/
 
-let li=document.getElementsByTagName('li');
-console.log(li);
-console.log(li[1]);
+
+//let li=document.getElementsByTagName('li');
+//console.log(li);
+//console.log(li[1]);
 /*
 if we add a new li tag without the same class name
 and we did the previous operations of getElementsByClassName
 nothing will happen but if we did the same thing with getElementsByTagName
 changes will be seen in the new li tag also
 */
+
+//queryselector
+//task-6
+/*
+let header=document.querySelector('#main-header');
+header.style.borderBottom='solid 4px #ccc';
+let input=document.querySelector('input');
+input.value='Hello world';
+
+let submit=document.querySelector('input[type="submit"]');
+submit.value='SEND';
+
+let item=document.querySelector('.list-group-item');
+item.style.color='red';
+
+let lastitem=document.querySelector('.list-group-item:last-child');
+lastitem.style.color='blue';
+
+let seconditem=document.querySelector('.list-group-item:nth-child(2)');
+seconditem.style.backgroundColor='green';
+
+let thirditem=document.querySelector('.list-group-item:nth-child(3)');
+thirditem.style.visibility='hidden';
+*/
+//queryselectorall
+
+let titles=document.querySelectorAll('.title');
+console.log(titles);
+//titles[0].textContent='Hello';
+
+//titles[1].style.color='green';
+
+let items=document.querySelectorAll('.list-group-item');
+items[1].style.color='green';
+
+let odd=document.querySelectorAll('li:nth-child(odd)');
+let even=document.querySelectorAll('li:nth-child(even)');
+for(let i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='green';
+    even[i].style.backgroundColor='#f4f4f4';
+}
