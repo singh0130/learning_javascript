52f1480ea6f15faaded4967a5eb62f71e6980478
singh0130 <mr.mohitsinghdev@gmail.com>
2022-03-29 08:24:22 +0530

    dom task 7 solution added

--- dom.js
+++ dom.js
@@ -83,12 +83,13 @@ thirditem.style.visibility='hidden';
 */
 //queryselectorall
 
-let titles=document.querySelectorAll('.title');
-console.log(titles);
+//let titles=document.querySelectorAll('.title');
+//console.log(titles);
 //titles[0].textContent='Hello';
 
 //titles[1].style.color='green';
 
+/*
 let items=document.querySelectorAll('.list-group-item');
 items[1].style.color='green';
 
@@ -98,3 +99,75 @@ for(let i=0;i<odd.length;i++){
     odd[i].style.backgroundColor='green';
     even[i].style.backgroundColor='#f4f4f4';
 }
+*/
+
+//traversing the dom
+let itemList=document.querySelector('#items');
+//parentnode
+//console.log(itemList.parentNode);
+//itemList.parentNode.style.backgroundColor='#f4f4f4';
+//console.log(itemList.parentNode.parentNode.parentNode);
+
+//parentElement
+/*
+console.log(itemList.parentElement);
+itemList.parentElement.style.backgroundColor='#f4f4f4';
+console.log(itemList.parentElement.parentElement.parentElement);
+*/
+
+//childNodes
+//console.log(itemList.childNodes); //not preferred to use as it inculdes line breaks as text also in the ListNode
+/*
+console.log(itemList.children); //it returns HTMLCollection but no line breaks as text
+console.log(itemList.children[1]);
+itemList.children[1].style.backgroundColor='yellow';
+*/
+//firstchild
+//console.log(itemList.firstChild); //use-less
+//firstElementChild
+//console.log(itemList.firstElementChild);
+//itemList.firstElementChild.textContent='Hello 1';
+
+//lastchild
+//console.log(itemList.lastChild); //use-less
+//lastElementChild
+//console.log(itemList.lastElementChild);
+//itemList.lastElementChild.textContent='Hello last';
+
+//nextSibling
+//console.log(itemList.nextSibling);
+//nextElementSibling
+//console.log(itemList.nextElementSibling);
+
+//previousSibling
+//console.log(itemList.previousSibling.previousSibling);
+//previousElementSibling
+//console.log(itemList.previousElementSibling);
+//itemList.previousElementSibling.style.color='green';
+
+//createElement
+
+//create a div
+let newDiv=document.createElement('div');
+//Add class
+newDiv.className='hello';
+//Add id
+newDiv.id='Hello1';
+//Add attribute
+newDiv.setAttribute('title','Hello Div');
+//create text node
+let newDivText=document.createTextNode('Hello world');
+//Add text to div
+newDiv.appendChild(newDivText);
+
+let container=document.querySelector('header .container');
+let h1=document.querySelector('header h1');
+container.insertBefore(newDiv,h1);
+newDiv.style.fontSize='30px';
+
+console.log(newDiv);
+
+let ul=document.querySelector('ul');
+let li=document.querySelector('li');
+
+ul.insertBefore(newDiv,li);
